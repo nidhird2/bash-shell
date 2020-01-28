cdeb5edc9e14e5c789f059e0e866765859ec97a8
Nidhi Dholaria <nidhird2@illinois.edu>
2020-01-28 10:33:04 -0600

    added multi punc test

--- extreme_edge_cases/camelCaser_tests.c
+++ extreme_edge_cases/camelCaser_tests.c
@@ -22,14 +22,16 @@ int findSize(char** actual){
 int checkIfMatch(char** actual, char** expected){
     int len = findSize(actual);
     int len2 = findSize(expected);
+//    printf("act len1: %d\n", len);
+//    printf("exp len2: %d\n", len2);
     if(len != len2){
 	return 0;
     }
     int i = 0;
 //    printf("len: %d\n", len);
     for(i = 0; i < len; i++){
-	//printf("ac: %s\n", actual[i]);
-	//printf("ex: %s\n", expected[i]);
+//	printf("ac: %s\n", actual[i]);
+//	printf("ex: %s\n", expected[i]);
 	if(strcmp(actual[i], expected[i]) != 0){
 		return 0;
 	}
@@ -60,7 +62,7 @@ int checkOneSentence(char **(*camelCaser)(const char*), void (*destroy)(char**))
     return result;
 }
 
-int check3(char**(*camelCaser)(const char*), void (*destroy)(char**)){
+int noSpaces(char**(*camelCaser)(const char*), void (*destroy)(char**)){
     char* input = "faslfjsljlkxn,mcngsaAGSSDF";
     char* exp[] ={"faslfjsljlkxn", NULL};
     char **output = camelCaser(input);
@@ -72,8 +74,8 @@ int check3(char**(*camelCaser)(const char*), void (*destroy)(char**)){
     return result;
 }
 
-int check4(char**(*camelCaser)(const char*), void(*destroy)(char**)){
-    char* input = "  spaceSpace space     . Space space.";
+int whitespace(char**(*camelCaser)(const char*), void(*destroy)(char**)){
+    char* input = "  spaceSpace space  !   	 Space space.";
     char* exp[] = {"spacespaceSpace","spaceSpace",NULL};
     char **output = camelCaser(input);
     int result = 1;
@@ -84,7 +86,7 @@ int check4(char**(*camelCaser)(const char*), void(*destroy)(char**)){
     return result;
     }
 
-int check5(char**(*camelCaser)(const char*), void(*destroy)(char**)){
+int checkEmpty(char**(*camelCaser)(const char*), void(*destroy)(char**)){
     char* input = "";
     char* exp[] = {NULL};
     char **output = camelCaser(input);
@@ -94,14 +96,50 @@ int check5(char**(*camelCaser)(const char*), void(*destroy)(char**)){
     }
     destroy(output);
     return result;
-    }    
+    }
+
+int checkAllSpaces(char **(*camelCaser)(const char*), void(*destroy)(char**)){
+    char* input = "	   	";
+    char* exp[] = {NULL};
+    char **output = camelCaser(input);
+    int result = 1;
+    if(!checkIfMatch(output, exp)){
+	result = 0;
+    }
+    destroy(output);
+    return result;
+    }
+
+int checkAllNums(char **(*camelCaser)(const char*), void(* destroy)(char**)){
+    char* input = " 1 2 3 	. 4567 89. 012.  345 678.";
+    char* exp[] = {"123", "456789", "012", "345678", NULL};
+    char **output = camelCaser(input);
+    int result = 1;
+    if(!checkIfMatch(output,exp)){
+	result = 0;
+    }
+    destroy(output);
+    return result;
+    }
+
+int checkMultPunc(char **(*camelCaser)(const char*), void(* destroy)(char**)){
+    char* input = "  abc dEF  . ! ? gHiJ\",. KLMNO..";
+    char* exp[] = {"abcDef","","","ghij","","","klmno","",NULL};
+    char **output = camelCaser(input);
+    int result = 1;
+    if(!checkIfMatch(output,exp)){
+	result = 0;
+    }
+    destroy(output);
+    return result;
+    }
 
 int test_camelCaser(char **(*camelCaser)(const char *),
                     void (*destroy)(char **)) {
 
    int result = 1;
 
-   if(!checkNull(camelCaser, destroy)){
+  if(!checkNull(camelCaser, destroy)){
 	printf("checkNull failed.\n");
 	return 0;
    }
@@ -110,18 +148,31 @@ int test_camelCaser(char **(*camelCaser)(const char *),
 	return 0;
    }
 
-   if(!check3(camelCaser,destroy)){
-	printf("check3: no sentences failed.\n");
+   if(!noSpaces(camelCaser,destroy)){
+	printf("noSpaces: no sentences failed.\n");
 	return 0;
    }
   
-   if(!check4(camelCaser, destroy)){
-	printf("check4: failed\n");
+   if(!whitespace(camelCaser, destroy)){
+	printf("check4:whitespace failed\n");
 	return 0;
    }
 
-   if(!check5(camelCaser, destroy)){
+   if(!checkEmpty(camelCaser, destroy)){
 	printf("empty string input failed\n");
+	return 0;
+   }
+   if(!checkAllSpaces(camelCaser,destroy)){
+	printf("all spaces input failed\n");
+	return 0;
+   }
+   if(!checkAllNums(camelCaser, destroy)){
+	printf("check all nums input failed\n");
+	return 0;
+   }
+
+   if(!checkMultPunc(camelCaser, destroy)){
+	printf("multpunc input failed\n");
 	return 0;
    }
    return result;

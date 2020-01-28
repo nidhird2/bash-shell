629b64c93592d697578fb7c214862776e2fec618
Nidhi Dholaria <nidhird2@illinois.edu>
2020-01-27 20:12:35 -0600

    null + 1 sentence tests & functionality

--- extreme_edge_cases/camelCaser.c
+++ extreme_edge_cases/camelCaser.c
@@ -4,13 +4,102 @@
  */
 #include "camelCaser.h"
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int countSentences(const char* input);
+int countChars(const char* input);
+
+int countSentences(const char *input){
+    int count = 0;
+    const char* current = input;
+    while(*current){
+	if(ispunct(*current)){
+		count++;
+	}
+	current++;
+    }
+    return count;
+}
+
+int countChars(const char *input){
+    int count = 0;
+    int running = 0;
+    const char* current = input;
+    while(*current){
+	if(isspace(*current)){
+		current++;
+		continue;
+	}
+	else if(ispunct(*current)){
+		count += running + 1;
+		running = 0;
+		current++;
+	}
+	else{
+		running++;
+		current++;
+	}
+    }
+    return count;
+}
 
 char **camel_caser(const char *input_str) {
     // TODO: Implement me!
-    return NULL;
+    char** result = NULL;
+    if(input_str == NULL){
+	return result;
+    }
+    int num_sentences = countSentences(input_str);
+    int totalChars = countChars(input_str);
+    char* wordSpace = (char*)malloc(totalChars);
+    result = (char**)malloc(sizeof(char*)*(num_sentences+1));
+    const char* current = input_str;
+    char* currentSpace = wordSpace;
+    int isPreviousSpace = 1;
+    int isFirstWord = 1;
+    int currentIdx = 0;
+    while(*current){
+	if(isspace(*current)){
+		isPreviousSpace = 1;
+		current++;
+		continue;
+	}
+	else if(ispunct(*current)){
+		*currentSpace = '\0';
+		currentSpace++;
+		result[currentIdx] = wordSpace;
+		currentIdx++;
+		wordSpace = currentSpace;
+		isFirstWord = 1;
+		isPreviousSpace = 1;
+		current++;
+	}
+	else{
+		if(isPreviousSpace && (!isFirstWord)){
+			*currentSpace = (char)toupper(*current);
+		}
+		else{
+			*currentSpace = (char)tolower(*current);
+		}
+		isFirstWord = 0;
+		isPreviousSpace = 0;
+		currentSpace++;
+		current++; 
+	}
+    }
+    result[currentIdx] = NULL;
+    return result;
+    
 }
 
 void destroy(char **result) {
     // TODO: Implement me!
+    if(result == NULL){
+	return;
+    }
+    free(result[0]);
+    free(result);
+    result = NULL;
     return;
 }
--- extreme_edge_cases/camelCaser_tests.c
+++ extreme_edge_cases/camelCaser_tests.c
@@ -9,8 +9,56 @@
 #include "camelCaser.h"
 #include "camelCaser_tests.h"
 
+int checkIfMatch(char** actual, char** expected){
+    printf("Here2 \n");
+    if(sizeof(actual) != sizeof(expected)){
+	return 0;
+    }
+    int len = sizeof(actual);
+    int i = 0;
+    for(i = 0; i < len - 1; i++){
+	if(strcmp(actual[i], expected[i]) != 0){
+		return 0;
+	}
+    }
+    printf("Here3 \n");
+    return 1;
+}
+
+int checkNull(char **(*camelCaser)(const char *), void (*destroy)(char**)){
+    char* input = NULL;
+    char** output = camelCaser(input);
+    int result = 1;
+    if(output != NULL){
+	result = 0;
+    }
+    destroy(output);
+    return result;
+}
+
+int checkOneSentence(char **(*camelCaser)(const char*), void (*destroy)(char**)){
+    char* input = "hello world.";
+    char* exp[] = {"helloWord", NULL};
+    char **output = camelCaser(input);
+    int result = 1;
+    if(checkIfMatch(output, exp)){
+	result = 0;
+    }
+    destroy(output);
+    return result;
+}
+    
+
 int test_camelCaser(char **(*camelCaser)(const char *),
                     void (*destroy)(char **)) {
-    // TODO: Implement me!
-    return 1;
+   int result = 1;
+   if(!checkNull(camelCaser, destroy)){
+	printf("checkNull failed.\n");
+	return 0;
+   }
+   if(!checkOneSentence(camelCaser, destroy)){
+	printf("check one sentence failed.\n");
+	result = 0;
+   }
+   return result;
 }

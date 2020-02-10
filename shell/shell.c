187b9f5f54eee0fef71f665c030f1578a1192b95
Nidhi Dholaria <nidhird2@illinois.edu>
2020-02-10 17:25:59 -0600

    internal built in done

--- shell/shell.c
+++ shell/shell.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <signal.h> 
 
 typedef struct process {
     char *command;
@@ -17,43 +18,50 @@ typedef struct process {
 
 static vector* history;
 
-int input_loop(void);
-int handle_input(char*);
-int do_cd(char*);
-int cleanup_and_exit(void);
-int print_history(void);
-int print_history_idx(size_t);
+void input_loop(void);
+void handle_input(char*);
+void do_cd(char*);
+void cleanup_and_exit(void);
+void print_history(void);
+void print_history_idx(size_t);
+void caught_sigint();
+void history_match(char*);
 
 int shell(int argc, char *argv[]) {
     // TODO: This is the entry point for your shell.
+    signal(SIGINT, caught_sigint); 
     history = string_vector_create();
     input_loop();
     return 0;
 }
 
-int input_loop(){
+void input_loop(){
     pid_t my_pid = getpid();
     while(1){
         char* cur_dir = get_current_dir_name();
         print_prompt(cur_dir,my_pid);
+        free(cur_dir);
         char* s = NULL;
         size_t n = 0;
         int read = getline(&s, &n, stdin);
         if(read == -1){
+            free(s);
             cleanup_and_exit();
         }
-        handle_input(s);
-        free(cur_dir);
+        char* copy = (char*)malloc(strlen(s) + 1);
+        strcpy(copy, s);
+        free(s);
+        handle_input(copy);
     }
-    return 1;
+    return;
 }
 
-int handle_input(char* input){
+void handle_input(char* input){
     //printf("%s\n", input);
     int len = strlen(input);
     if(len == 0){
         free(input);
-        return 0;
+        return;
     }
     if(input[len - 1] == '\n'){
         input[len - 1] = '\0';
@@ -83,13 +91,16 @@ int handle_input(char* input){
         size_t idx;
         sscanf(copy, "#%lu", &idx);
         print_history_idx(idx);
-    } 
+    }
+    else if(copy[0] == '!'){
+        history_match(copy);
+    }
     free(input);
     free(copy);
-    return 0;
+    return;
 }
 
-int do_cd(char* s){
+void do_cd(char* s){
     if(s == NULL){
         print_no_directory("");
     }
@@ -97,31 +108,56 @@ int do_cd(char* s){
     if(res == -1){
         print_no_directory(s);
     }
-    return 0;
+    return;
 }
 
-int print_history(void){
+void history_match(char* prefix){
+    size_t his_size = vector_size(history);
+
+    for(size_t i = his_size; i --> 0 ;){
+        char* his = (char*)vector_get(history, i);
+        //printf("i: %lu\n", i);
+        //printf("his: %s\n", his);
+        //printf("prefix: %s\n", prefix + 1);
+        if(strstr(his, prefix+1) != NULL) {
+            //printf("HERE\n");
+            char* match = his;
+            char* copy = malloc(strlen(match) + 1);
+            strcpy(copy, match);
+            handle_input(copy);
+            return;
+        }
+    }
+    print_no_history_match();
+    return;
+}
+
+void print_history(void){
     size_t his_size = vector_size(history);
     for(size_t idx = 0; idx < his_size; idx++){
-        print_history_line(idx, vector_get(history, idx));
+        print_history_line(idx, (char*)vector_get(history, idx));
     }
-    return 0;
+    return;
 }
 
-int print_history_idx(size_t idx){
+void print_history_idx(size_t idx){
     if(idx >= vector_size(history)){
         print_invalid_index();
     }
     else{
-        char* match = vector_get(history, idx);
+        char* match = (char*)vector_get(history, idx);
         char* copy = malloc(strlen(match) + 1);
         strcpy(copy, match);
         handle_input(copy);
     }
-    return 0;
+    return;
 }
 
-int cleanup_and_exit(void){
+void caught_sigint(){
+    cleanup_and_exit();
+}
+
+void cleanup_and_exit(void){
     vector_destroy(history);
     exit(1);
 }

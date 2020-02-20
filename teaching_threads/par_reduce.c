014c457187c85e2407fbfec4f24d102aa7c4f327
Nidhi Dholaria <nidhird2@illinois.edu>
2020-02-20 13:44:34 -0600

    initial: limited testing

--- teaching_threads/par_reduce.c
+++ teaching_threads/par_reduce.c
@@ -12,11 +12,69 @@
 #include "reducers.h"
 
 /* You might need a struct for each task ... */
+typedef struct thread_task{
+    size_t start_idx;
+    size_t end_idx;
+    int* list;
+    reducer func;
+    int base_case;
+} task_t;
+
+void* reduce_thread(void* input){
+    task_t* task = (task_t*)input;
+    size_t length = task->end_idx -task->start_idx + 1;
+    //printf("start_idx: %lu, end_idx: %lu, length: %lu\n", task->start_idx, task->end_idx, length);
+    int l[length];
+    for(size_t j = task->start_idx; j <= task->end_idx; j++){
+        l[j - task->start_idx] = task->list[j];
+    }
+    int* real_result = (int*)malloc(sizeof(int));
+    *real_result = reduce(l, length, task->func, task->base_case);
+    //*real_result = 0;
+    return real_result;
+}
 
 /* You should create a start routine for your threads. */
 
 int par_reduce(int *list, size_t list_len, reducer reduce_func, int base_case,
                size_t num_threads) {
     /* Your implementation goes here */
-    return 0;
+    size_t new_length;
+    size_t max_use_threads = list_len / 2;
+    if(num_threads > max_use_threads){
+        new_length = max_use_threads;
+    }
+    else{
+        new_length = num_threads;
+    }
+    int new_list[new_length];
+    pthread_t tids[new_length];
+    task_t tasks[new_length];
+    size_t each_size = list_len / new_length;
+    //need to divide tasks to threads
+    for(size_t i = 0; i < new_length; i++){
+        tasks[i].list = list;
+        tasks[i].base_case = base_case;
+        tasks[i].func = reduce_func;
+        if(i + 1 != new_length){
+            tasks[i].start_idx= i * each_size;
+            tasks[i].end_idx = tasks[i].start_idx + each_size - 1;
+        } else{
+            tasks[i].start_idx = i * each_size;
+            tasks[i].end_idx = list_len - 1;
+        }
+        //printf("start_idx: %lu, end_idx: %lu\n", tasks[i].start_idx, tasks[i].end_idx);
+        //printf("new length: %lu\n", tasks[i].end_idx -tasks[i].start_idx + 1);
+        pthread_create(tids + i, NULL, reduce_thread, (void*)(tasks + i));
+    }
+    //join thread results & store in new list
+    for(size_t i = 0; i < new_length; i++){
+        void* result;
+        pthread_join(tids[i], &result);
+        new_list[i] = *(int*)result;
+        free(result);
+    }
+
+    //combine thread results by reducing as normal
+    return reduce(new_list, new_length, reduce_func, base_case);
 }

2d49f88790da636e7a3eae8e241b410860f1c00b
Nidhi Dholaria <nidhird2@illinois.edu>
2020-02-21 00:33:06 -0600

    reducded copying, making it faster

--- teaching_threads/par_reduce.c
+++ teaching_threads/par_reduce.c
@@ -13,23 +13,25 @@
 
 /* You might need a struct for each task ... */
 typedef struct thread_task{
-    size_t start_idx;
-    size_t end_idx;
+    //size_t start_idx;
+    //size_t end_idx;
     int* list;
     reducer func;
     int base_case;
+    size_t length;
 } task_t;
 
 void* reduce_thread(void* input){
     task_t* task = (task_t*)input;
-    size_t length = task->end_idx -task->start_idx + 1;
+    //size_t length = task->end_idx -task->start_idx + 1;
     //printf("start_idx: %lu, end_idx: %lu, length: %lu\n", task->start_idx, task->end_idx, length);
-    int l[length];
-    for(size_t j = task->start_idx; j <= task->end_idx; j++){
-        l[j - task->start_idx] = task->list[j];
-    }
+    //int l[length];
+    // for(size_t j = task->start_idx; j <= task->end_idx; j++){
+    //     l[j - task->start_idx] = task->list[j];
+    // }
     int* real_result = (int*)malloc(sizeof(int));
-    *real_result = reduce(l, length, task->func, task->base_case);
+    //*real_result = reduce(task->list, , task->func, task->base_case);
+    *real_result = reduce(task->list, task->length, task->func, task->base_case);
     //*real_result = 0;
     return real_result;
 }
@@ -39,6 +41,9 @@ void* reduce_thread(void* input){
 int par_reduce(int *list, size_t list_len, reducer reduce_func, int base_case,
                size_t num_threads) {
     /* Your implementation goes here */
+    if(num_threads == 1){
+        return reduce(list, list_len, reduce_func, base_case);
+    }
     size_t new_length;
     size_t max_use_threads = list_len / 2;
     if(num_threads > max_use_threads){
@@ -53,15 +58,17 @@ int par_reduce(int *list, size_t list_len, reducer reduce_func, int base_case,
     size_t each_size = list_len / new_length;
     //need to divide tasks to threads
     for(size_t i = 0; i < new_length; i++){
-        tasks[i].list = list;
         tasks[i].base_case = base_case;
         tasks[i].func = reduce_func;
+        tasks[i].list = list + i * each_size;
         if(i + 1 != new_length){
-            tasks[i].start_idx= i * each_size;
-            tasks[i].end_idx = tasks[i].start_idx + each_size - 1;
+            //tasks[i].start_idx= i * each_size;
+            //tasks[i].end_idx = tasks[i].start_idx + each_size;
+            tasks[i].length = each_size;
         } else{
-            tasks[i].start_idx = i * each_size;
-            tasks[i].end_idx = list_len - 1;
+            //tasks[i].start_idx = i * each_size;
+            //tasks[i].end_idx = list_len - 1;
+            tasks[i].length = each_size + (list_len % new_length);
         }
         //printf("start_idx: %lu, end_idx: %lu\n", tasks[i].start_idx, tasks[i].end_idx);
         //printf("new length: %lu\n", tasks[i].end_idx -tasks[i].start_idx + 1);

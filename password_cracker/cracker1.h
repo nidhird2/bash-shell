31dbd9d3de13c7cd455c5740cdae0cd90b26c229
Maciej Krzysiak <mkrzys2@illinois.edu>
2020-03-08 15:26:50 -0500

    Deploying password_cracker

---
+++ password_cracker/cracker1.h
@@ -0,0 +1,13 @@
+/**
+ * Password Cracker
+ * CS 241 - Spring 2020
+ */
+/**
+ * this file exists so that we can provide a main method which does a variety of
+ * handy things
+ */
+
+#include <stddef.h>
+
+// entry point for student code
+int start(size_t thread_count);
